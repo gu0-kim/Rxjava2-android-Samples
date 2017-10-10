05ff06fa2ad0505532872f1dbe96174ba2fe33b6
gloriousgu <gloriousgu@126.com>
2017-10-10 23:00:26 +0800

    modify by gloriousgu

--- app/src/main/java/com/rxjava2/android/samples/ui/operators/ZipExampleActivity.java
+++ app/src/main/java/com/rxjava2/android/samples/ui/operators/ZipExampleActivity.java
@@ -3,7 +3,6 @@ package com.rxjava2.android.samples.ui.operators;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -40,12 +39,7 @@ public class ZipExampleActivity extends AppCompatActivity {
         btn = (Button) findViewById(R.id.btn);
         textView = (TextView) findViewById(R.id.textView);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                testWork();
-            }
-        });
+        btn.setOnClickListener(view -> testWork());
     }
 
     /*
